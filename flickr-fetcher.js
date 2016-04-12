ee7880cd97e250b937d14e73deec921969001687
James Sinclair <jsinclair@squiz.net>
2016-04-12 19:59:05 +1000

    Initial commit

---
+++ flickr-fetcher.js
@@ -0,0 +1,36 @@
+'use strict';
+var FlickrFetcher;
+
+FlickrFetcher = {
+    photoObjToURL: function(photoObj) {
+        return [ 'https://farm',
+            photoObj.farm, '.staticflickr.com/', 
+            photoObj.server, '/',
+            photoObj.id, '_',
+            photoObj.secret, '_b.jpg'
+        ].join('');
+    },
+
+    transformPhotoObj: function(photoObj) {
+        return {
+            title: photoObj.title,
+            url:   FlickrFetcher.photoObjToURL(photoObj)
+        };
+    },
+
+    fetchFlickrData: function(apiKey, fetch) {
+        var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key='
+                + apiKey.toString() + '&text=kittens&format=json&nojsoncallback=1'
+        return fetch(url);
+    },
+
+    fetchPhotos: function(apiKey, fetch) {
+        return FlickrFetcher.fetchFlickrData(apiKey, fetch).then(function(data) {
+            return data.photos.photo.map(FlickrFetcher.transformPhotoObj);
+        });
+    }
+};
+
+if ((typeof module !== 'undefined') && (typeof module.exports !== 'undefined')) {
+    module.exports = FlickrFetcher;
+}
